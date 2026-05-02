4fd546d1946c789bc577ab040dfc4a84f88e3dd4
skyeas-creator <skylenyansiaboka@gmail.com>
2026-05-02 03:47:04 +0300

    Added Hello.java and main.java control flow example

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class main {
+    public static void main(String[]args){
+        int a=30;
+        int b=40;
+        int sum= a+b;
+        if(sum>50){
+            System.out.println("sum is greater than 50");
+
+        
+        }
+    }
+}
