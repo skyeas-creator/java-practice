18c2a751a4ba7c911728395523f65cbd37969598
skyeas-creator <skylenyansiaboka@gmail.com>
2026-05-02 04:10:50 +0300

    update main.java

--- Main.java
+++ Main.java
@@ -1,12 +1,14 @@
 public class main {
     public static void main(String[]args){
-        int a=30;
-        int b=40;
-        int sum= a+b;
+        int x=10;
+        int y=30;
+        int sum= x+y;
         if(sum>50){
             System.out.println("sum is greater than 50");
-
+        }else
+            {
+            System.out.println("sum is less than 50");
+  }
         
         }
     }
-}
